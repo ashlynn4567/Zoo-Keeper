ab86449744bce87137e26114adee54ebd648d2c3
Ashley Smith <ashlynn4567@gmail.com>
2022-04-18 13:29:19 -0500

    added basic query parameter handling and port variable to server.js. Also uploaded json data

---
+++ server.js
@@ -0,0 +1,56 @@
+// import express into project so we can use express framework
+const express = require("express");
+// import json data about animals
+const { animals } = require("./data/animals");
+
+
+const PORT = process.env.PORT || 3001;
+// instantiate the server
+const app = express();
+
+// setting up queries for which users can request data using specific search parameters
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+
+    if (query.personalityTraits) {
+        if (typeof query.personalityTraits === "string") {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        };
+        // loop through each trait in the personalityTraits array
+        personalityTraitsArray.forEach(trait => {
+            // for each queried trait, the filteredResults array will only contain 
+            // entries containing the queried trait. At the end, we'll have an array
+            // of animals that have every one of the queried traits
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    };
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    };
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    };
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    };
+    return filteredResults;
+};
+
+// add route for json data
+app.get("/api/animals", (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    };
+    res.json(animals);
+});
+
+// tell the server to listen for requests in port 3001
+app.listen(3001, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
