af871f5ed8c705fb8e71d2f208cf6e5af750bea8
Ashley's PC <ashlynn4567@gmail.com>
2022-05-01 18:56:48 -0500

    bugfixes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,4 +1,5 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 const animalRoutes = require("../apiRoutes/animalRoutes");
 const zookeeperRoutes = require("../apiRoutes/zookeeperRoutes");
 
